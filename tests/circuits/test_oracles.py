e8ce21f29286857c9610a0e049030ce8db3d64e6
Isha Janbakhsh <ij251@cam.ac.uk>
2025-05-25 21:04:24 +0100

    wrote dj algorithm test

--- tests/circuits/test_oracles.py
+++ tests/circuits/test_oracles.py
@@ -1,13 +1,13 @@
 from qiskit import QuantumCircuit
 from arqana.circuits.oracles import oracle_balanced, oracle_constant
 
-def test_balanced_oracle_qubit_count():
+def test_balanced_oracle():
     qc = QuantumCircuit(3)
     qc2 = oracle_balanced(qc, 3)
     assert isinstance(qc2, QuantumCircuit)
     assert qc2.num_qubits == 3
 
-def test_constant_oracle_type():
+def test_constant_oracle():
     qc = QuantumCircuit(3)
     qc2 = oracle_constant(qc, 3)
     assert isinstance(qc2, QuantumCircuit)
